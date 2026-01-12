32543f44a067048c1baae5a4a751e99d7d8f8a99
Mateusz Kłusek <mateuszklusek@outlook.com>
2026-01-12 11:52:06 +0100

    chore: add type safety to pluralization template

--- library/src/copy/utils.ts
+++ library/src/copy/utils.ts
@@ -3,8 +3,15 @@ import type { QuantityRangeRule } from "@shared/src/types/copy";
 type Props = {
   quantity: number;
   rules: QuantityRangeRule;
-  template?: string;
+  template?: PluralizationTemplate;
 };
+
+/**
+ * A string that must contain both {quantity} and {label}
+ */
+type PluralizationTemplate =
+  | `${string}{quantity}${string}{label}${string}`
+  | `${string}{label}${string}{quantity}${string}`;
 
 export const handlePluralization = ({
   quantity,
