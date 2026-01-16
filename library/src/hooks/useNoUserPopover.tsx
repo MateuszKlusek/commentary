fc2578793243cec45f22f74f301ba14fcc28ef67
Mateusz Kłusek <mateuszklusek@outlook.com>
2026-01-16 02:28:22 +0100

    chore: update the styling for NoUserPopover

--- library/src/hooks/useNoUserPopover.tsx
+++ library/src/hooks/useNoUserPopover.tsx
@@ -1,19 +1,28 @@
 import * as Popover from "@radix-ui/react-popover";
 import { useCallback, useState } from "react";
+import { useCopy } from "../context/CopyContext";
 
 export const useNoUserPopover = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const {
+    noUserTooltip: { title, description, buttonLabel },
+  } = useCopy();
 
   const NoUserPopover = useCallback(
     ({ children }: { children: React.ReactNode }) => {
       return (
         <Popover.Root open={isOpen} onOpenChange={setIsOpen}>
           <Popover.Anchor>{children}</Popover.Anchor>
-          <Popover.Content side="left" avoidCollisions>
-            <div className="p-4 bg-red-500">
-              <h3>Want to join the conversation?</h3>
-              <p>Sign in to continue</p>
-              <button>Sign in</button>
+          <Popover.Content side="bottom" align="start" avoidCollisions>
+            <div className="p-6 bg-[#212121] rounded-3xl gap-2 flex flex-col items-center">
+              <h3 className="text-[#ffffff] text-[20px] font-bold">{title}</h3>
+              <p className="text-[#AAAAAA] text-[14px] font-normal">
+                {description}
+              </p>
+              <button className="text-[#0f0f0f] text-[14px] font-medium bg-[#f1f1f1] rounded-full px-4 py-2 mt-2 w-full cursor-pointer hover:bg-[#e1e1e1]">
+                {buttonLabel}
+              </button>
             </div>
           </Popover.Content>
         </Popover.Root>
