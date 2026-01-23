cb7cdc97c0aba5f60f1306323e0669684221f147
Mateusz Kłusek <mateuszklusek@outlook.com>
2026-01-23 16:28:04 +0100

    chore: add json types for seed jsons into shared

---
+++ shared/src/types/json.ts
@@ -0,0 +1,28 @@
+export type JsonCommentStats = {
+  commentId: string;
+  likeCount: number;
+  dislikeCount: number;
+  replyCount: number;
+};
+
+export type JsonComment = {
+  discussionId: string;
+  userId: string;
+  parentId: string | null;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type JsonUserReaction = {
+  userId: string;
+  commentId: string;
+  reaction: 1 | -1 | 0;
+  createdAt: string;
+};
+
+export type JsonUser = {
+  userId: string;
+  name: string;
+  avatarUrl: string;
+};
