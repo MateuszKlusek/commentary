6ed8add77eea699b6c9952aaecbd33607160ad90
Mateusz Kłusek <mateuszklusek@outlook.com>
2026-01-25 19:41:27 +0100

    chore: use connection with error handling for the pool

--- server/src/services/psql.service.ts
+++ server/src/services/psql.service.ts
@@ -29,20 +29,22 @@ export class PsqlCommentaryServiceSingleton implements CommentaryActionsWithDisc
     },
   ) {
     const pool = getPool();
+    const connect = await pool.connect();
     const { limit, offset, sortBy, userId, parentId } = params;
 
-    const countResult = await pool.query<{ total: string }>(
-      `
+    try {
+      const countResult = await connect.query<{ total: string }>(
+        `
       SELECT COUNT(*) as total
       FROM comments
       WHERE discussion_id = $1 AND parent_id IS NOT DISTINCT FROM $2
       `,
-      [discussionId, parentId],
-    );
-    const itemsCount = parseInt(countResult.rows[0].total, 10);
+        [discussionId, parentId],
+      );
+      const itemsCount = parseInt(countResult.rows[0].total, 10);
 
-    const result = await pool.query(
-      `
+      const result = await connect.query(
+        `
       SELECT
         c.comment_id as comment_id,
         c.discussion_id,
@@ -66,40 +68,45 @@ export class PsqlCommentaryServiceSingleton implements CommentaryActionsWithDisc
       ORDER BY c.created_at ${sortBy === "newest" ? "DESC" : "ASC"}
       LIMIT $4 OFFSET $5
       `,
-      [discussionId, userId, parentId, limit, offset],
-    );
+        [discussionId, userId, parentId, limit, offset],
+      );
 
-    const rows: CommentItem[] = result.rows.map((row) => {
-      return {
-        comment: {
-          commentId: row.comment_id,
-          discussionId: row.discussion_id,
-          userId: row.user_id,
-          parentId: row.parent_id,
-          content: row.content,
-          createdAt: row.created_at,
-          updatedAt: row.updated_at,
-        },
-        commentStats: {
-          commentId: row.comment_id,
-          likeCount: row.like_count,
-          dislikeCount: row.dislike_count,
-          replyCount: row.reply_count,
-        },
-        author: {
-          userId: row.author_user_id,
-          name: row.author_name,
-          avatarUrl: row.author_avatar_url,
-        },
-        userReaction: {
-          commentId: row.comment_id,
-          userId: row.user_id,
-          reaction: row.user_reaction,
-        },
-      };
-    });
+      const rows: CommentItem[] = result.rows.map((row) => {
+        return {
+          comment: {
+            commentId: row.comment_id,
+            discussionId: row.discussion_id,
+            userId: row.user_id,
+            parentId: row.parent_id,
+            content: row.content,
+            createdAt: row.created_at,
+            updatedAt: row.updated_at,
+          },
+          commentStats: {
+            commentId: row.comment_id,
+            likeCount: row.like_count,
+            dislikeCount: row.dislike_count,
+            replyCount: row.reply_count,
+          },
+          author: {
+            userId: row.author_user_id,
+            name: row.author_name,
+            avatarUrl: row.author_avatar_url,
+          },
+          userReaction: {
+            commentId: row.comment_id,
+            userId: row.user_id,
+            reaction: row.user_reaction,
+          },
+        };
+      });
 
-    return { items: rows, itemsCount };
+      return { items: rows, itemsCount };
+    } catch (error) {
+      throw error;
+    } finally {
+      connect.release();
+    }
   }
   // ----------------------- CommentaryAPI methods ------------------------
 
@@ -144,29 +151,37 @@ export class PsqlCommentaryServiceSingleton implements CommentaryActionsWithDisc
     parentId: Nullable<string>,
   ) {
     const pool = getPool();
-    const commentId = crypto.randomUUID();
+    const connect = await pool.connect();
 
-    await pool.query("BEGIN");
-    const commentResult = await pool.query(
-      `
+    try {
+      const commentId = crypto.randomUUID();
+
+      await connect.query("BEGIN");
+      const commentResult = await connect.query(
+        `
       INSERT INTO comments (comment_id, discussion_id, user_id, parent_id, content)
       VALUES ($1, $2, $3, $4, $5)
       `,
-      [commentId, discussionId, userId, parentId, content],
-    );
-
-    await pool.query(
-      `
+        [commentId, discussionId, userId, parentId, content],
+      );
+      await connect.query(
+        `
       INSERT INTO comment_stats (comment_id, like_count, dislike_count, reply_count)
       VALUES ($1, 0, 0, 0)
       ON CONFLICT (comment_id) 
       DO UPDATE SET reply_count = comment_stats.reply_count + 1
       `,
-      [parentId || commentId],
-    );
+        [parentId || commentId],
+      );
 
-    await pool.query("COMMIT");
-    return commentResult.rows[0];
+      await connect.query("COMMIT");
+      return commentResult.rows[0];
+    } catch (error) {
+      await connect.query("ROLLBACK");
+      throw error;
+    } finally {
+      connect.release();
+    }
   }
 }
 
