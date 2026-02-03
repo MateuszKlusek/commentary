c3a435240ccfb1702f172065ea1bb424554a22eb
Mateusz Kłusek <mateuszklusek@outlook.com>
2026-02-03 17:06:22 +0100

    chore: fix the incorrect useMemo import

--- library/src/hooks/useMergeAPIWithDefaults.ts
+++ library/src/hooks/useMergeAPIWithDefaults.ts
@@ -1,14 +1,10 @@
 import type { CommentaryAPI } from "@shared/src/types/core";
-import { useMemo } from "storybook/internal/preview-api";
+import { useMemo } from "react";
 
 export const useMergeAPIWithDefaults = (api: CommentaryAPI) => {
-  const mergedApi: CommentaryAPI = useMemo(
-    () => ({
-      ...{ mode: "development", validationMode: "warn" },
-      ...api,
-    }),
-    [api]
-  );
+  const mergedApi: CommentaryAPI = useMemo(() => {
+    return { ...{ mode: "development", validationMode: "warn" }, ...api };
+  }, [api]);
 
   return mergedApi;
 };
